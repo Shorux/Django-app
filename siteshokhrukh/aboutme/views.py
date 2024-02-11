c00a19d72b788d9fc44b9d397c197655e0c6d7c9
Shokhrukh <tolipovsh1994@gmail.com>
2024-02-11 19:00:20 +0500

    added handlers for archive and page_not_found url requests

--- siteshokhrukh/aboutme/views.py
+++ siteshokhrukh/aboutme/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 
 
@@ -6,5 +6,16 @@ def index(request: HttpRequest):
     return HttpResponse('Страница приложения aboutme')
 
 
-def blogs(request: HttpRequest):
-    return HttpResponse('<h1>Статьи</h1>')
+def blogs(request: HttpRequest, blog_id: int):
+    return HttpResponse(f'<h1>Статья</h1><p>id: {blog_id}</p>')
+
+
+def archive(request: HttpRequest, year: int):
+    if year > 2024:
+        raise Http404()
+
+    return HttpResponse(f'<h1>Архив из {year} года</h1>')
+
+
+def page_not_found(request: HttpRequest, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
