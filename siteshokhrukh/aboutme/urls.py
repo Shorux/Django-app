3bfaf5a09d85f31e0b00e55c0ae6381ae8001aee
Shokhrukh <tolipovsh1994@gmail.com>
2024-02-11 20:03:11 +0500

    realized redirect functionality

--- siteshokhrukh/aboutme/urls.py
+++ siteshokhrukh/aboutme/urls.py
@@ -7,7 +7,8 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
-    path('', views.index),
-    path('blogs/<int:blog_id>/', views.blogs),
-    path('archive/<year4:year>/', views.archive)
+    path('', views.index, name='home'),
+    path('blogs/<int:blog_id>/', views.blogs_by_id, name='blog_id'),
+    path('blogs/<slug:blog_slug>/', views.blogs_by_slug, name='blog_slug'),
+    path('archive/<year4:year>/', views.archive, name='archive')
 ]
--- siteshokhrukh/aboutme/views.py
+++ siteshokhrukh/aboutme/views.py
@@ -1,18 +1,24 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def index(request: HttpRequest):
     return HttpResponse('Страница приложения aboutme')
 
 
-def blogs(request: HttpRequest, blog_id: int):
+def blogs_by_id(request: HttpRequest, blog_id: int):
     return HttpResponse(f'<h1>Статья</h1><p>id: {blog_id}</p>')
+
+
+def blogs_by_slug(request: HttpRequest, blog_slug: int):
+    return HttpResponse(f'<h1>Статья</h1><p>slug: {blog_slug}</p>')
 
 
 def archive(request: HttpRequest, year: int):
     if year > 2024:
-        raise Http404()
+        uri = reverse('blog_slug', args=('music', ))
+        return redirect(uri)  # permanent=True for 301 else 302
 
     return HttpResponse(f'<h1>Архив из {year} года</h1>')
 
