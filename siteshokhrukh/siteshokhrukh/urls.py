1d8aac62d35852686cd12dd238f92442a5cd9589
Shokhrukh <tolipovsh1994@gmail.com>
2024-02-11 19:01:00 +0500

    added 404 handler

--- siteshokhrukh/siteshokhrukh/urls.py
+++ siteshokhrukh/siteshokhrukh/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from aboutme.views import page_not_found
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('aboutme.urls'))
 ]
+
+handler404 = page_not_found
